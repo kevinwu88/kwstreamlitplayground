111de351ea113ff8acbec993b825eae0af9a129d
Kevin Wu <kevin@vivatech.com.au>
2024-06-13 13:57:10 +1000

    Added logo

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from openai import OpenAI
 
+ICON_URL = 'https://www.fieldko.com/hubfs/320x132px-trans-01%20(14).png';
+
 # with st.sidebar:
 #     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 #     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
 #     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
 #     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("😈 Kevin's Chatbot")
+st.logo(LOGO_URL_LARGE, link=ICON_URL, icon_image=LOGO_URL_SMALL)
+st.title("FieldKo Copilot Assistant")
 
 openai_api_key = st.secrets["openai_api_key"]
 
